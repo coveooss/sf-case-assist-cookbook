3df9f9c863a583cc0c1943220c455bd9b18c3fcb
MehdiMitiche <gm_mitiche@esi.dz>
2021-11-27 21:31:33 -0500

    SFINT-4125 required proprety added

--- src/main/default/lwc/descriptionInput/descriptionInput.js
+++ src/main/default/lwc/descriptionInput/descriptionInput.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 import explainProblem from '@salesforce/label/c.cookbook_DescriptionInputLabel';
+import errorValueMissing from '@salesforce/label/c.cookbook_ValueMissing';
 
 /**
  * The `descriptionInput` component displays a rich text input for the case description.
@@ -8,7 +9,8 @@ import explainProblem from '@salesforce/label/c.cookbook_DescriptionInputLabel';
  */
 export default class DescriptionInput extends LightningElement {
   labels = {
-    explainProblem
+    explainProblem,
+    errorValueMissing
   };
 
   /**
@@ -18,8 +20,25 @@ export default class DescriptionInput extends LightningElement {
    */
   @api label = this.labels.explainProblem;
 
+  /**
+   * Tells if the input is required.
+   * @type {boolean}
+   * @defaultValue `false`
+   */
+  @api required = false;
+
+  /**
+   * The error message to be shown when the value is missing.
+   * @type {string}
+   * @defaultValue `'Complete this field.`
+   */
+  @api messageWhenValueMissing = this.labels.errorValueMissing;
+
   /** @type {string} */
   _value = '';
+
+  /** @type {string} */
+  _errorMessage = '';
 
   /**
    * List of formats to include in the editor.
@@ -38,9 +57,38 @@ export default class DescriptionInput extends LightningElement {
     'header'
   ];
 
-  handleChange(e) {
-    this._value = e.target.value;
+  /**
+   * Returns the error message to be shown.
+   * @type {string}
+   */
+  get errorMessage() {
+    return this._errorMessage;
   }
+
+  /**
+   * Returns the CSS class of the form.
+   * @returns {string}
+   */
+  get formClass() {
+    return `slds-form-element ${this.hasError ? 'slds-has-error' : ''}`;
+  }
+
+  /**
+   * Tells if there is an error in the input.
+   * @returns {boolean}
+   */
+  @api get hasError() {
+    return !!this._errorMessage.length;
+  }
+
+  /**
+   * Handles the changes in the input.
+   * @return {void}
+   */
+  handleChange = (e) => {
+    this._errorMessage = '';
+    this._value = e.target.value;
+  };
 
   /**
    * Returns the value of the input.
@@ -48,5 +96,15 @@ export default class DescriptionInput extends LightningElement {
    */
   @api get value() {
     return this._value;
+  }
+
+  /**
+   * Shows an error message in the componet if there is an error.
+   * @returns {void}
+   */
+  @api reportValidity() {
+    if (this.required && !this._value) {
+      this._errorMessage = this.messageWhenValueMissing;
+    }
   }
 }

56361c0b9540cb12b09cf7404aeec6257013c897
MehdiMitiche <gm_mitiche@esi.dz>
2022-01-22 23:57:59 -0500

    SFINT-4131 unit tests added

--- src/main/default/lwc/createCaseButton/createCaseButton.js
+++ src/main/default/lwc/createCaseButton/createCaseButton.js
@@ -1,6 +1,7 @@
 import { LightningElement, api } from 'lwc';
-import { createCase } from 'c/ratingUtils';
+import { createCase as newCase } from 'c/caseUtils';
 import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+import createCase from '@salesforce/label/c.cookbook_CreateCase';
 
 /**
  * @typedef CaseData
@@ -13,31 +14,40 @@ import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
  */
 
 export default class CreateCaseButton extends LightningElement {
+  labels = {
+    createCase
+  };
+
   /**
    * @type {CaseData}
    */
   @api caseData;
   /**
-   * The size of the button
+   * The size of the button.
    * @type {'small'|'big'}
    */
   @api size = 'big';
   /**
-   * The label of the button
+   * The label displayed above the button.
    * @type {string}
    */
   @api label = '';
   /**
+   * The label displayed inside the button.
    * @type {string}
    */
-  @api buttonLabel = 'Create case';
+  @api buttonLabel = this.labels.createCase;
 
   get buttonClass() {
     return `slds-button slds-button_brand ${this.size === 'big' ? 'big' : ''}`;
   }
 
+  get showLabel() {
+    return !!this.label.length;
+  }
+
   async handleCreateCase() {
-    const newCaseId = await createCase(this.caseData);
+    const newCaseId = await newCase(this.caseData);
     const attributeChangeEvent = new FlowAttributeChangeEvent(
       'recordId',
       newCaseId

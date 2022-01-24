ceb827f2842ee5f765e9194d14214bd33b339b88
MehdiMitiche <gm_mitiche@esi.dz>
2022-01-24 11:30:11 -0500

    SFINT-4128 analytics added to navigation buttons

--- src/main/default/lwc/navigationButton/navigationButton.js
+++ src/main/default/lwc/navigationButton/navigationButton.js
@@ -1,6 +1,12 @@
 import { LightningElement, api } from 'lwc';
 import next from '@salesforce/label/c.cookbook_Next';
 import previous from '@salesforce/label/c.cookbook_Previous';
+import {
+  registerComponentForInit,
+  initializeWithHeadless
+} from 'c/quanticHeadlessLoader';
+
+/** @typedef {import("coveo").CaseAssistEngine} CaseAssistEngine */
 
 /**
  * The `navigationButton` is a button to navigate through the steps of the case creation process.
@@ -13,6 +19,12 @@ export default class NavigationButton extends LightningElement {
     previous
   };
 
+  /**
+   * The ID of the case assist engine instance the component registers to.
+   * @api
+   * @type {string}
+   */
+  @api engineId;
   /**
    * The type of the navigation button.
    * @type {'next'|'previous'}
@@ -32,6 +44,29 @@ export default class NavigationButton extends LightningElement {
    *
    */
   @api showIcon = false;
+
+  /** @type {CaseAssistEngine} */
+  engine;
+
+  connectedCallback() {
+    registerComponentForInit(this, this.engineId);
+  }
+
+  renderedCallback() {
+    initializeWithHeadless(this, this.engineId, this.initialize);
+  }
+
+  /**
+   * @param {CaseAssistEngine} engine
+   */
+  initialize = (engine) => {
+    this.engine = engine;
+
+    this.actions = {
+      // eslint-disable-next-line no-undef
+      ...CoveoHeadlessCaseAssist.loadCaseAssistAnalyticsActions(engine)
+    };
+  };
 
   /**
    * Returns the label to be shown in the navigation button.
@@ -67,6 +102,7 @@ export default class NavigationButton extends LightningElement {
   handleNavigate() {
     if (this.type === 'next') {
       this.dispatchEvent(new CustomEvent('next', { bubbles: true }));
+      this.engine.dispatch(this.actions.logCaseNextStage());
     } else {
       this.dispatchEvent(new CustomEvent('previous', { bubbles: true }));
     }

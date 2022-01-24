ebe0fd198d9c6cb23f64f80d5cfd0e044b679d49
MehdiMitiche <gm_mitiche@esi.dz>
2022-01-24 08:23:38 -0500

    SFINT-4131 logCreateCase added

--- src/main/default/lwc/createCaseButton/createCaseButton.js
+++ src/main/default/lwc/createCaseButton/createCaseButton.js
@@ -2,6 +2,12 @@ import { LightningElement, api } from 'lwc';
 import { createCase as newCase } from 'c/caseUtils';
 import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
 import createCase from '@salesforce/label/c.cookbook_CreateCase';
+import {
+  registerComponentForInit,
+  initializeWithHeadless
+} from 'c/quanticHeadlessLoader';
+
+/** @typedef {import("coveo").CaseAssistEngine} CaseAssistEngine */
 
 /**
  * @typedef CaseData
@@ -16,13 +22,19 @@ import createCase from '@salesforce/label/c.cookbook_CreateCase';
 /**
  * The `createCaseButton` component is a button to submit the create of a case.
  * @example
- * <c-create-case-button button-label="Create case" label="Still need help" size="big"></c-create-case-button>
+ * <c-create-case-button eingine-id={engineId} button-label="Create case" label="Still need help" size="big"></c-create-case-button>
  */
 export default class CreateCaseButton extends LightningElement {
   labels = {
     createCase
   };
 
+  /**
+   * The ID of the engine instance the component registers to.
+   * @api
+   * @type {string}
+   */
+  @api engineId;
   /**
    * @type {CaseData}
    */
@@ -43,6 +55,29 @@ export default class CreateCaseButton extends LightningElement {
    */
   @api buttonLabel = this.labels.createCase;
 
+  /** @type {CaseAssistEngine} */
+  engine;
+
+  connectedCallback() {
+    registerComponentForInit(this, this.engineId);
+  }
+
+  renderedCallback() {
+    initializeWithHeadless(this, this.engineId, this.initialize);
+  }
+
+  /**
+   * @param {CaseAssistEngine} engine
+   */
+  initialize = (engine) => {
+    this.engine = engine;
+
+    this.actions = {
+      // eslint-disable-next-line no-undef
+      ...CoveoHeadlessCaseAssist.loadCaseAssistAnalyticsActions(engine)
+    };
+  };
+
   get buttonClass() {
     return `slds-button slds-button_brand ${this.size === 'big' ? 'big' : ''}`;
   }
@@ -53,14 +88,17 @@ export default class CreateCaseButton extends LightningElement {
 
   async handleCreateCase() {
     const newCaseId = await newCase(this.caseData);
-    const attributeChangeEvent = new FlowAttributeChangeEvent(
-      'recordId',
-      newCaseId
-    );
-    const customEvent = new CustomEvent('next', {
-      bubbles: true
-    });
-    this.dispatchEvent(attributeChangeEvent);
-    this.dispatchEvent(customEvent);
+    if (newCaseId) {
+      this.engine.dispatch(this.actions.logCaseStart(newCaseId));
+      const attributeChangeEvent = new FlowAttributeChangeEvent(
+        'recordId',
+        newCaseId
+      );
+      const customEvent = new CustomEvent('next', {
+        bubbles: true
+      });
+      this.dispatchEvent(attributeChangeEvent);
+      this.dispatchEvent(customEvent);
+    }
   }
 }

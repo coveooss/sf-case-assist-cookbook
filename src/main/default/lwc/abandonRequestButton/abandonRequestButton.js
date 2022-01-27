ebbe4a7b9bd3e135b5614d27873e47810d36da09
MehdiMitiche <gm_mitiche@esi.dz>
2022-01-27 07:55:55 -0500

    SFINT-4132 redirectRef added to abandonRequestButton component

--- src/main/default/lwc/abandonRequestButton/abandonRequestButton.js
+++ src/main/default/lwc/abandonRequestButton/abandonRequestButton.js
@@ -33,6 +33,16 @@ export default class AbandonRequestButton extends LightningElement {
    * @type {string}
    */
   @api buttonLabel = this.labels.abandonRequest;
+  /**
+   * Object describing the type of page, its attributes, and the state of the page to redirect to when abandoning the case, more information in https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.use_navigate_basic
+   * @type {object}
+   */
+  @api redirectRef = {
+    type: 'standard__namedPage',
+    attributes: {
+      pageName: 'home'
+    }
+  };
 
   get buttonClass() {
     return `slds-button slds-button_outline-brand ${

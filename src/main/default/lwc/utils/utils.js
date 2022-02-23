658272653b62f4c27421fd9f71b29daf7b731ee4
MehdiMitiche <gm_mitiche@esi.dz>
2022-02-23 08:31:04 -0500

    unused files deleted

--- src/main/default/lwc/utils/utils.js
+++ src/main/default/lwc/utils/utils.js
@@ -6,27 +6,3 @@ export function debounce(fn, wait) {
     timeout = setTimeout(functionCall, wait); // eslint-disable-line @lwc/lwc/no-async-operation
   };
 }
-
-export function getVisitorId() {
-  let visitorId;
-  try {
-    if (window.coveoanalytics) {
-      // If you have the AnalyticsBeacon LWC in your community, it will have generated a visitorId for you by now.
-      visitorId = window.coveoanalytics.storage
-        .getAvailableStorage()
-        .getItem('visitorId');
-    }
-
-    if (!visitorId) {
-      // Try to read it directly from localStorage.
-      visitorId = localStorage.getItem('visitorId');
-    }
-    if (!visitorId) {
-      console.warn('Cannot find visitorId from the community');
-    }
-  } catch (err) {
-    console.warn('Cannot find visitorId from the community');
-  }
-
-  return visitorId;
-}
